66a2de828b374a65e4c951d0c7bcf870c58d63f9
kevinmore <dingfengyu@gmail.com>
2014-02-21 12:06:37 +0000

    first try

---
+++ PlusOne.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        int i;
+        for(i=digits.size()-1;i>=0;--i){
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                ++digits[i];
+                return digits;
+            }
+        }
+        if(i<0) digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
